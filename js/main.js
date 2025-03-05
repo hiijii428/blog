f10a81abdf2fa0ab7bec1ac382d87a2659d9b0de
hiijii428 <hiijii428@gmail.com>
2025-03-05 16:11:03 +0900

    new blog

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//fetch("https://hiijii428.github.com/blog/side.html")
+$.ajax({
+	type: 'GET',
+    // 取得したいページのアドレス
+	url: './side.html',
+	success: function(data) {
+        document.querySelector("#nav").innerHTML = data;
+	}
+});
+
+const sub = document.querySelector("iframe")
+function side_iframe_setting() {
+    sub.style.width = sub.contentWindow.document.body.scrollWidth + "px";
+    sub.style.height = sub.contentWindow.document.body.scrollHeight+25 + "px";
+}
+
+function code_title(){
+	let elm = document.querySelectorAll("code");
+    console.log(elm)
+	for(let i=0;i<elm.length;i++){
+		let now_elm = elm[i]
+		let code_name = now_elm.className.replace("language-","").replace(" hljs","");
+        console.log(i,now_elm.classList.value)
+		if(code_name){
+			if(code_name==="cpp"){code_name="c++";}
+			if(code_name==="csharp"){code_name="C#";}
+			let code_name_html = "<div class='code_name'>"+code_name+"</div>";
+            console.log(code_name_html)
+			now_elm.innerHTML = code_name_html;
+            //  + now_elm.innerHTML.replace(code_name_html,"");
+		}
+	}
+}
+let markdown_str = document.querySelector(".markdown");
+markdown_str.innerHTML = marked.parse(markdown_str.innerHTML);
+hljs.initHighlightingOnLoad();
+code_title();
